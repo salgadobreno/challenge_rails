a7d45a7f7e8edcee2307f5633171dbf1a1c3f580
Breno Salgado <salgado.breno@gmail.com>
2025-02-02 17:08:49 -0300

    test: cart_routing_spec pass

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
+  resource :cart do
+    post "add_item" => "carts#add_item"
+  end
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "rails/health#show"
